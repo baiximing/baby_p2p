d3a80ebade2b79949139f63ead18680300bf031d
baiximing <1683685916@qq.com>
2020-01-14 19:15:22 +0800

    主页面的跳转登陆，注册跳转，还要共有页面显示的调整，dao模块新增了MyBatis-Puls的接口。

---
+++ babydao/src/main/java/com/dao/user/UserMapper.java
@@ -0,0 +1,6 @@
+package com.dao.user;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends  BaseMapper {
+}
--- babyuser/src/main/java/com/babyuser/controller/UserController.java
+++ babyuser/src/main/java/com/babyuser/controller/UserController.java
@@ -7,9 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    /**
+     * 进入前台主页面
+     * @return
+     */
     @RequestMapping("/index")
-    public String Index(){
+    public String ToIndex(){
         System.out.println("进入前台主页面——————————");
         return "index";
+    }
+
+    /**
+     * 进入登陆页面
+     * @return
+     */
+    @RequestMapping("/login")
+    public String ToLogin(){
+        System.out.println("进入登陆页面——————————");
+        return "login";
+    }
+
+    /**
+     * 进入注册页面
+     * @return
+     */
+    @RequestMapping("/register")
+    public String ToRegister(){
+        System.out.println("进入注册页面——————————");
+        return "register";
     }
 }
